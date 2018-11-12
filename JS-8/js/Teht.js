70d094caf78da9f90a446f3b47d7094e2e41386b
OliverOstman <olivero@metropolia.fi>
2018-11-12 12:34:51 +0200

    JS-8

--- JS-8/js/Teht.js
+++ JS-8/js/Teht.js
@@ -1,3 +1,13 @@
+'use strict';
+for (let i=0; i<5; i++) {
+  const body = document.getElementsByTagName('body')[0];
+  const image = '<img src="">';
+  const text = '<p></p>';
+  body.innerHTML = body.innerHTML + text;
+  document.getElementsByTagName('p')[i].innerHTML = image;
+  let p = document.getElementsByTagName('p')[i];
+}
+
 const nappi = document.querySelector('#hakunappi');
 nappi.addEventListener('click', function(evt) {
   let etsi = document.querySelector('#hakuteksti').value;
@@ -12,81 +22,70 @@ nappi.addEventListener('click', function(evt) {
 })
 
 
-
 function etsiminen(look) {
   for (let i=0; i<5; i++) {
-    const body = document.getElementsByTagName('body')[0];
-
+    let nimi, kuvaus, linkki, kotisivu, kuva;
     const image = '<img src="">';
-    const p = '<p></p>';
-    body.innerHTML = body.innerHTML + p;
-    document.getElementsByTagName('p')[i].innerHTML = '';
+    let p = document.getElementsByTagName('p')[i];
+    p.innerHTML = image;
 
-    if (look[i].show.name === null) {
-      const nimi = "NULL";
+    p.innerHTML = p.innerHTML + "<br>" + (1 + i) + "/5" + "<br>";
+
+    if (look[i].show.name === undefined) {
+      nimi = null;
     } else {
-      const nimi = look[i].show.name;
+      nimi = look[i].show.name;
+    }
+    p.innerHTML = p.innerHTML + "<br>" + 'Ohjelman nimi: ' + nimi + "<br>";
+
+    if (look[i].show.url === undefined || look[i].show.url === null) {
+      linkki = "-";
+      p.innerHTML = p.innerHTML + 'Ohjelma linkki: ' + linkki + "<br>";
+    } else {
+      linkki = look[i].show.url;
+      p.innerHTML = p.innerHTML + 'Ohjelma linkki: ' + linkki.link(linkki) + "<br>";
     }
 
-    if (look[i].show.summary === null) {
-      const kuvaus = "NULL";
+    if (look[i].show.officialSite === undefined || look[i].show.officialSite === null) {
+      kotisivu = "-";
+      p.innerHTML = p.innerHTML + 'Ohjelma kotisivu: ' + kotisivu + "<br><br>";
     } else {
-      const kuvaus = look[i].show.summary;
+      kotisivu = look[i].show.officialSite;
+      p.innerHTML = p.innerHTML + 'Ohjelma kotisivu: ' + kotisivu.link(kotisivu) + "<br><br>";
     }
 
-    if (look[i].show.url === null) {
-      const linkki = "NULL";
-    } else {
-      const linkki = look[i].show.url;
-    }
 
-    if (look[i].show.officialSite === null) {
-      const kotisivu = "NULL";
+    if (look[i].show.image === null || look[i].show.image.medium === undefined) {
+      kuva = "";
     } else {
-      const kotisivu = look[i].show.officialSite;
+      kuva = look[i].show.image.medium;
     }
+    document.getElementsByTagName('img')[i].src = kuva;
 
-    if (look[i].show.image.medium === null) {
-      const kuva = "NULL";
+    if (look[i].show.summary === undefined) {
+      kuvaus = null;
     } else {
-      const kuva = look[i].show.image.medium;
+      kuvaus = look[i].show.summary;
+      console.log(kuvaus);
+      p.innerHTML = p.innerHTML + 'Ohjelman kuvaus: ' /* + kuvaus */  + "<br><br>";
     }
 
     const genres = look[i].show.genres.length;
 
-    /*let i, s;
-    s = 2;
-    const body = document.getElementsByTagName('body')[0];
-    for (i=0; i<=2; i++) {
-      const image = '<img class="list">';
-      const list = '<li></li>';
-      body.innerHTML = list + body.innerHTML;
-      document.getElementsByTagName('li')[0].innerHTML = image;
-      document.getElementsByTagName('img')[0].src = pics[s].thumb;
-      s--;
-    }
-    const bigImg = document.getElementsByTagName('img')[3];
-    const img1 = document.getElementsByTagName('img')[0];
-    const img2 = document.getElementsByTagName('img')[1];
-    const img3 = document.getElementsByTagName('img')[2]; */
-
-    /* let image = document.createElement('img');
-    let p = document.createElement('p'); */
+    /*
+    p.innerHTML = i+1 + "/5" + "<br>" + p.innerHTML + "<br>" + 'Ohjelman nimi: ' + nimi + "<br>"
+        + 'Ohjelma linkki: ' + linkki.link(linkki) + "<br>"
+        + 'Ohjelma kotisivu: ' + kotisivu.link(kotisivu) + "<br><br>"; */
 
 
-      p.innerHTML = p.innerHTML + 'Ohjelman nimi: ' + nimi + "<br>";
+    /* p.innerHTML = p.innerHTML + 'Ohjelman kuvaus: ' + kuvaus + "<br>"; */
 
     for (let s=0; s<genres; s++) {
       const genre = look[i].show.genres[s];
-      p.innerHTML = p.innerHTML + 'Ohjelman genret: ' + genre + "<br>";
+      p.innerHTML = p.innerHTML + 'Ohjelman genre ' + (1 + s) + ": " + genre + "<br>";
     }
 
-    p.innerHTML = p.innerHTML + 'Ohjelma linkki: ' + linkki.link(linkki) +
-        "<br>";
-    p.innerHTML = p.innerHTML + 'Ohjelma kotisivu: ' + kotisivu.link(kotisivu) +
-        "<br><br>";
-    p.innerHTML = p.innerHTML + 'Ohjelman kuvaus: ' + kuvaus;
-    image.src = kuva;
-
+    /* let image = document.createElement('img');
+    let p = document.createElement('p'); */
   }
 }
